a454e94496c60f214fc62de4b7ff25bb54b4db1f
yuichiinami <66859207+yuichiinami@users.noreply.github.com>
2020-07-21 22:32:01 +0900

    非同期通信

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `<div class="message-lists">
+        <div class="message-list">
+          <div class="message-list__name">
+            ${message.user_name}
+          </div>
+          <div class="message-list__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-comment">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="message-lists">
+        <div class="message-list">
+          <div class="message-list__name">
+            ${message.user_name}
+          </div>
+          <div class="message-list__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message-comment">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('input').prop('disabled', false);
+    }) 
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('input').prop('disabled', false);
+    });  
+})  
+});
